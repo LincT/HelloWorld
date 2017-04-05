fa367dc5e894cf8dba47d21cfd31f1ba83c46d97
LincT <joseph.dwr.martin@gmail.com>
2017-04-05 10:56:11 -0500

    initial
    Java Hello World Message Box

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//System.out.println("Hello world!");
+JOptionPane.showMessageDialog(null,"Hello World!\n");
+	}
+
+}
